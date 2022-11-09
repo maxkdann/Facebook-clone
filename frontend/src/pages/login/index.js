66905e590d753104e72c92a980810318b1f09de8
maxkdann <maxkdann@hotmail.com>
2022-11-09 12:05:16 -0500

    added error handling to login page

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -20,7 +20,7 @@ export default function Login() {
       .required("Email address is required.")
       .email("Must be a valid email.")
       .max(100),
-    passowrd: Yup.string().required("Password is required."),
+    password: Yup.string().required("Password is required."),
   });
 
   return (
@@ -56,6 +56,7 @@ export default function Login() {
                       type="password"
                       name="password"
                       onChange={handleLoginChange}
+                      bottom
                     />
                     <button type="submit" className="blue_btn">
                       Log In
@@ -76,6 +77,57 @@ export default function Login() {
           </div>
         </div>
         <div className="register"></div>
+        <footer className="login_footer">
+          <div className="login_footer_wrap">
+            <Link to="/">English(UK)</Link>
+            <Link to="/">Francais(FR)</Link>
+            <Link to="/">Espanol(Espana)</Link>
+            <Link to="/">italiano</Link>
+            <Link to="/">Deutsch</Link>
+            <Link to="/" className="footer_square">
+              <i className="plus_icon"></i>
+            </Link>
+          </div>
+          <div className="footer_splitter"></div>
+          <div className="login_footer_wrap">
+            <Link to="/">Sign Up</Link>
+            <Link to="/">Log In</Link>
+            <Link to="/">Messenger</Link>
+            <Link to="/">Facebook Lite</Link>
+            <Link to="/">Watch</Link>
+            <Link to="/">Places</Link>
+            <Link to="/">Games</Link>
+            <Link to="/">Marketplace</Link>
+            <Link to="/">Facebook Pay</Link>
+            <Link to="/">Oculus</Link>
+            <Link to="/">Portal</Link>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Bulletin</Link>
+            <Link to="/">Local</Link>
+            <Link to="/">Fundraisers</Link>
+            <Link to="/">Services</Link>
+            <Link to="/">Voting Information Centre</Link>
+            <Link to="/">Groups</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Create ad</Link>
+            <Link to="/">Create Page</Link>
+            <Link to="/">Developers</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Privacy</Link>
+            <Link to="/">Cookies</Link>
+            <Link to="/">
+              AdChoices
+              <i className="adChoices_icon"></i>
+            </Link>
+            <Link to="/">Terms</Link>
+            <Link to="/">Help</Link>
+          </div>
+          <div className="login_footer_wrap">
+            <Link to="/" style={{ fontSize: "12px", marginTop: "10px" }}>
+              Meta © 2022
+            </Link>
+          </div>
+        </footer>
       </div>
     </div>
   );
