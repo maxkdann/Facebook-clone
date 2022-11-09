7d85d1a28fbdb35023f7dd2c6d08c0ded8100ece
maxkdann <maxkdann@hotmail.com>
2022-11-09 16:44:28 -0500

    working on sign up modal

---
+++ frontend/src/components/login/RegisterForm.js
@@ -0,0 +1,213 @@
+import { Form, Formik } from "formik";
+import { useState } from "react";
+import RegisterInput from "../inputs/registerInput";
+import * as Yup from "yup";
+export default function RegisterForm() {
+  const userInfos = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    bYear: new Date().getFullYear(),
+    bMonth: new Date().getMonth() + 1,
+    bDay: new Date().getDate(),
+    gender: "",
+  };
+  const [user, setUser] = useState(userInfos);
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    bYear,
+    bMonth,
+    bDay,
+    gender,
+  } = user;
+  const yearTemp = new Date().getFullYear();
+  const handleRegisterChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  const years = Array.from(new Array(108), (val, index) => yearTemp - index);
+  const months = Array.from(new Array(12), (val, index) => 1 + index);
+  const getDays = () => {
+    return new Date(bYear, bMonth, 0).getDate();
+  };
+  const days = Array.from(new Array(getDays()), (val, index) => 1 + index);
+  const registerValidation = Yup.object({
+    first_name: Yup.string()
+      .required("What's your first name?")
+      .min(2, "First name must be between 2 and 30 characters.")
+      .max(30, "First name must be between 2 and 30 characters.")
+      .matches(
+        /^[aA-zZ\s]+$/,
+        "Numbers and special characters are not allowed."
+      ),
+    last_name: Yup.string()
+      .required("What's your last name?")
+      .min(2, "First name must be between 2 and 30 characters.")
+      .max(30, "First name must be between 2 and 30 characters.")
+      .matches(
+        /^[aA-zZ\s]+$/,
+        "Numbers and special characters are not allowed."
+      ),
+    email: Yup.string()
+      .required(
+        "You'll need this when you log in and if your ever need to reset your password."
+      )
+      .email("Enter a valid email address."),
+    password: Yup.string()
+      .required(
+        "Enter a combination of at least six numbers, letters and punctuation marks (such as ! and &)"
+      )
+      .min(6, "Passowrd must be at least 6 characters")
+      .max(36, "Password cannot be longer than 36 characters"),
+  });
+  return (
+    <div className="blur">
+      <div className="register">
+        <div className="register_header">
+          <i className="exit_icon"></i>
+          <span>Sign Up</span>
+          <span>It's quick and easy</span>
+        </div>
+        <Formik
+          enableReinitialize
+          initialValues={{
+            first_name,
+            last_name,
+            email,
+            password,
+            bYear,
+            bMonth,
+            bDay,
+            gender,
+          }}
+          validationSchema={registerValidation}
+        >
+          {(formik) => (
+            <Form className="register_form">
+              <div className="reg_line">
+                <RegisterInput
+                  type="text"
+                  placeholder="First name"
+                  name="first_name"
+                  onChange={handleRegisterChange}
+                />
+                <RegisterInput
+                  type="text"
+                  placeholder="Surname"
+                  name="last_name"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_lin">
+                <RegisterInput
+                  type="text"
+                  placeholder="Mobile number or email address"
+                  name="email"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_lin">
+                <RegisterInput
+                  type="password"
+                  placeholder="New password"
+                  name="password"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_col">
+                <div className="reg_line_header">
+                  Date of birth <i className="info_icon"></i>
+                </div>
+                <div className="reg_grid">
+                  <select
+                    name="bDay"
+                    value={bDay}
+                    onChange={handleRegisterChange}
+                  >
+                    {days.map((day, i) => (
+                      <option value={day} key={i}>
+                        {day}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    name="bMonth"
+                    onChange={handleRegisterChange}
+                    value={bMonth}
+                  >
+                    {months.map((month, i) => (
+                      <option value={month} key={i}>
+                        {month}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    name="bYear"
+                    onChange={handleRegisterChange}
+                    value={bYear}
+                  >
+                    {years.map((year, i) => (
+                      <option value={year} key={i}>
+                        {year}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+              <div className="reg_col">
+                <div className="reg_line_header">
+                  Gender <i className="info_icon"></i>
+                </div>
+                <div className="reg_grid">
+                  <label htmlFor="male">
+                    Male
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="male"
+                      value="male"
+                      onChange={handleRegisterChange}
+                    ></input>
+                  </label>
+                  <label htmlFor="female">
+                    Female
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="female"
+                      value="female"
+                      onChange={handleRegisterChange}
+                    ></input>
+                  </label>
+                  <label htmlFor="custom">
+                    Custom
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="custom"
+                      value="custom"
+                      onChange={handleRegisterChange}
+                    ></input>
+                  </label>
+                </div>
+              </div>
+              <div className="reg_infos">
+                By clicking Sign Up, you agree to our{" "}
+                <span>Terms, Data Policy &nbsp;</span>
+                and <span>Cookie Policy.</span> You may receive SMS
+                notifications from us and can opt out at any time.
+              </div>
+              <div className="reg_btn_wrapper">
+                <button className="blue_btn open_signup">Sign Up</button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,133 +1,15 @@
 import "./style.css";
-import { Formik, Form } from "formik";
-import { Link } from "react-router-dom";
-import LoginInput from "../../components/inputs/loginInput";
-import { useState } from "react";
-import * as Yup from "yup";
-const loginInfos = {
-  email: "",
-  password: "",
-};
-export default function Login() {
-  const [login, setLogin] = useState(loginInfos);
-  const { email, password } = login;
-  const handleLoginChange = (e) => {
-    const { name, value } = e.target;
-    setLogin({ ...login, [name]: value });
-  };
-  const loginValidation = Yup.object({
-    email: Yup.string()
-      .required("Email address is required.")
-      .email("Must be a valid email.")
-      .max(100),
-    password: Yup.string().required("Password is required."),
-  });
+import LoginForm from "../../components/login/LoginForm";
+import Footer from "../../components/login/Footer";
+import RegisterForm from "../../components/login/RegisterForm";
 
+export default function Login() {
   return (
     <div className="login">
       <div className="login_wrapper">
-        <div className="login_wrap">
-          <div className="login_1">
-            <img src="../../icons/facebook.svg" alt="" />
-            <span>
-              Facebook helps you connect and share with the people in your life.
-            </span>
-          </div>
-          <div className="login2">
-            <div className="login_2_wrap">
-              <Formik
-                enableReinitialize
-                initialValues={{
-                  email,
-                  password,
-                }}
-                validationSchema={loginValidation}
-              >
-                {(formik) => (
-                  <Form>
-                    <LoginInput
-                      type="text"
-                      name="email"
-                      placeholder="Email address or phone number"
-                      onChange={handleLoginChange}
-                    />
-                    <LoginInput
-                      placeholder="Password"
-                      type="password"
-                      name="password"
-                      onChange={handleLoginChange}
-                      bottom
-                    />
-                    <button type="submit" className="blue_btn">
-                      Log In
-                    </button>
-                  </Form>
-                )}
-              </Formik>
-              <Link to="/forgot" className="forgot_password">
-                Forgot your password?
-              </Link>
-              <div className="sign_splitter"></div>
-              <button className="blue_btn open_signup">Create Account</button>
-            </div>
-            <Link to="/" clasName="sign_extra">
-              <b>Create a page </b>
-              for a celebrity, brand or business.
-            </Link>
-          </div>
-        </div>
-        <div className="register"></div>
-        <footer className="login_footer">
-          <div className="login_footer_wrap">
-            <Link to="/">English(UK)</Link>
-            <Link to="/">Francais(FR)</Link>
-            <Link to="/">Espanol(Espana)</Link>
-            <Link to="/">italiano</Link>
-            <Link to="/">Deutsch</Link>
-            <Link to="/" className="footer_square">
-              <i className="plus_icon"></i>
-            </Link>
-          </div>
-          <div className="footer_splitter"></div>
-          <div className="login_footer_wrap">
-            <Link to="/">Sign Up</Link>
-            <Link to="/">Log In</Link>
-            <Link to="/">Messenger</Link>
-            <Link to="/">Facebook Lite</Link>
-            <Link to="/">Watch</Link>
-            <Link to="/">Places</Link>
-            <Link to="/">Games</Link>
-            <Link to="/">Marketplace</Link>
-            <Link to="/">Facebook Pay</Link>
-            <Link to="/">Oculus</Link>
-            <Link to="/">Portal</Link>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Bulletin</Link>
-            <Link to="/">Local</Link>
-            <Link to="/">Fundraisers</Link>
-            <Link to="/">Services</Link>
-            <Link to="/">Voting Information Centre</Link>
-            <Link to="/">Groups</Link>
-            <Link to="/">About</Link>
-            <Link to="/">Create ad</Link>
-            <Link to="/">Create Page</Link>
-            <Link to="/">Developers</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Privacy</Link>
-            <Link to="/">Cookies</Link>
-            <Link to="/">
-              AdChoices
-              <i className="adChoices_icon"></i>
-            </Link>
-            <Link to="/">Terms</Link>
-            <Link to="/">Help</Link>
-          </div>
-          <div className="login_footer_wrap">
-            <Link to="/" style={{ fontSize: "12px", marginTop: "10px" }}>
-              Meta © 2022
-            </Link>
-          </div>
-        </footer>
+        <LoginForm />
+        <RegisterForm />
+        <Footer />
       </div>
     </div>
   );
