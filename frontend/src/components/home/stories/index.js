baf79755395191019aa0dd6ca752b9dacb0d9fcf
maxkdann <maxkdann@hotmail.com>
2022-11-22 14:53:14 -0500

    finished homepage responsiveness

--- frontend/src/components/home/stories/index.js
+++ frontend/src/components/home/stories/index.js
@@ -10,7 +10,21 @@ export default function Stories() {
   const query1030px = useMediaQuery({
     query: "(max-width: 1030px)",
   });
-  const max = query1030px ? 5 : query1175px ? 4 : stories.length;
+  const query960px = useMediaQuery({
+    query: "(max-width: 960px)",
+  });
+  const query885px = useMediaQuery({
+    query: "(max-width: 885px)",
+  });
+  const max = query885px
+    ? 5
+    : query960px
+    ? 4
+    : query1030px
+    ? 5
+    : query1175px
+    ? 4
+    : stories.length;
   return (
     <div className="stories">
       <div className="create_story_card">
